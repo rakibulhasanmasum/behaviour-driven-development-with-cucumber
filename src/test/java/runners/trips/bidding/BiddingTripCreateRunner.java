7ed7a8bee205bbc2451e19b5f657625d4c5eae3a
r_hasan <rakibul.hasan@trucklagbe.com>
2022-10-30 02:19:28 +0600

    early versions of reports generated

--- src/test/java/runners/trips/bidding/BiddingTripCreateRunner.java
+++ src/test/java/runners/trips/bidding/BiddingTripCreateRunner.java
@@ -7,7 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         glue = {"stepDefinitions.trips.bidding"},
-        features = "src/test/resources/functionalTests/trips/bidding/BiddingTripCreate.feature"
+        features = "src/test/resources/functionalTests/trips/bidding/BiddingTripCreate.feature",
+        plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
+                "junit:target/cucumber-reports/Cucumber.xml",
+                "html:target/cucumber-reports/Cucumber.html"},
+        monochrome = true
 )
 public class BiddingTripCreateRunner {
 }
