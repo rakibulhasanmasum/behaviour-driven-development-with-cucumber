98c01b5f016f6be11cfd55f3057ba91793309614
r_hasan <rakibul.hasan@trucklagbe.com>
2022-11-07 21:34:15 +0600

    refactored shipper pages

--- src/main/java/com/trucklagbe/pageObjects/shipper/details/UserRestrictionModal.java
+++
@@ -1,4 +0,0 @@
-package com.trucklagbe.pageObjects.shipper.details;
-
-public class UserRestrictionModal {
-}
--- src/main/java/com/trucklagbe/pageObjects/shipper/details/notifications/SendNotificationModal.java
+++
@@ -1,4 +0,0 @@
-package com.trucklagbe.pageObjects.shipper.details.notifications;
-
-public class SendNotificationModal {
-}
